781c4f8c315e2354c7e3b53c6edd217014257473
acxgray <nedmostoles@gmail.com>
2025-05-31 00:13:53 +0800

    initial commit

---
+++ src/test/java/com/nm/personal/financetracker/FinancetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nm.personal.financetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
